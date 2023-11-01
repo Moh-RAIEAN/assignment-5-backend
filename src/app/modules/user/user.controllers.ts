9e99a251f23205c2ee018b55112955b24ba0e528
mohhasibalraiean <hasibalraiean@gmail.com>
2023-11-01 08:20:00 +0600

    read user service,controller,route craeted

--- src/app/modules/user/user.controllers.ts
+++ src/app/modules/user/user.controllers.ts
@@ -1,3 +1,4 @@
+import { Types } from "mongoose";
 import sendResponse from "../../../helpers/sendResponseHelpers/sendResponse";
 import catchAsync from "../../shared/catchAsync";
 import { UserServices } from "./user.service";
@@ -12,4 +13,16 @@ const createUser = catchAsync(async (req, res) => {
   });
 });
 
-export const UserControllers = { createUser };
+const readUser = catchAsync(async (req, res) => {
+  const userId = req.params?.userId;
+  const result = await UserServices.readUser(
+    userId as unknown as Types.ObjectId,
+  );
+  sendResponse(res, {
+    statusCode: result?.statusCode,
+    message: result?.message,
+    data: result?.data,
+  });
+});
+
+export const UserControllers = { createUser, readUser };
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,6 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import { IGenericResult } from "../../../helpers/sendResponseHelpers/sendResponseHelpers.interface";
 import { IUser } from "./user.interface";
 import { User } from "./user.model";
+import { Types } from "mongoose";
 
 const createUser = async (
   userData: IUser,
@@ -9,9 +10,19 @@ const createUser = async (
   const result = await User.create(userData);
   return {
     statusCode: StatusCodes.CREATED,
-    message: "user created",
+    message: "user created successfully",
     data: result,
   };
 };
 
-export const UserServices = { createUser };
+const readUser = async (
+  userId: Types.ObjectId,
+): Promise<IGenericResult<IUser | null>> => {
+  const result = await User.findById(userId);
+  return {
+    statusCode: StatusCodes.CREATED,
+    message: "user retrived successfully",
+    data: result,
+  };
+};
+export const UserServices = { createUser, readUser };
