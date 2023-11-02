d7aa5d16af14d3e3a08b4662de6860bfbcaef791
mohhasibalraiean <hasibalraiean@gmail.com>
2023-11-02 06:52:33 +0600

    created zod vaidation for create book api

--- src/app/modules/book/book.routes.ts
+++ src/app/modules/book/book.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { BookControllers } from "./book.controller";
+import validateRequest from "../../middlewares/validateRequestHandler";
+import { BookValidations } from "./book.validation";
 
 const router: Router = Router();
 
-router.post("/", BookControllers.createBook);
+router.post(
+  "/",
+  validateRequest(BookValidations.createBookZodValitaionSchema),
+  BookControllers.createBook,
+);
 
 export const BookRoutes = router;
---
+++ src/app/modules/book/book.validation.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import { z } from "zod";
+
+const createBookZodValitaionSchema = z.object({
+  body: z
+    .object({
+      title: z.string().trim().toLowerCase(),
+      author: z.string().trim().toLowerCase(),
+      genre: z.string().trim().toLowerCase(),
+      publicationYear: z.string().trim().toLowerCase(),
+      user: z.custom<string>((value) => {
+        if (!value) {
+          throw new z.ZodError([
+            {
+              path: ["userId"],
+              code: "custom",
+              message: "user field is required!",
+            },
+          ]);
+        } else if (!mongoose.Types.ObjectId.isValid(value as string)) {
+          throw new z.ZodError([
+            { path: ["userId"], code: "custom", message: "invalid user" },
+          ]);
+        }
+        return value;
+      }),
+    })
+    .strict({
+      message:
+        "please provide only title, author, genre, publicationYear, user fileds only!",
+    }),
+});
+
+export const BookValidations = { createBookZodValitaionSchema };
